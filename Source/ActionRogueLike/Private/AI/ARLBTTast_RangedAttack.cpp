cbf58a1f1accf1a9fc3e1b4d0082391c91f9832b
fatihklc817 <mf.kilic@hotmail.com>
2023-10-09 18:57:44 +0300

    Ranged minion attack bttask added

---
+++ Source/ActionRogueLike/Private/AI/ARLBTTast_RangedAttack.cpp
@@ -0,0 +1,43 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AI/ARLBTTast_RangedAttack.h"
+
+#include "AIController.h"
+#include "BehaviorTree/BlackboardComponent.h"
+#include "GameFramework/Character.h"
+
+EBTNodeResult::Type UARLBTTast_RangedAttack::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+	
+	AAIController* myAiController = OwnerComp.GetAIOwner();
+	if (ensure(myAiController))
+	{
+		 ACharacter* MyPawn = Cast<ACharacter>(myAiController->GetPawn());
+		 if (MyPawn == nullptr)
+		 {
+			 return EBTNodeResult::Failed;
+		 }
+
+		FVector MuzzleLocation = MyPawn->GetMesh()->GetSocketLocation("Muzzle_01");
+
+		AActor* TargetActor = Cast<AActor>(OwnerComp.GetBlackboardComponent()->GetValueAsObject("TargetActor"));
+		 if (TargetActor == nullptr)
+		 {
+			 return EBTNodeResult::Failed;
+		 }
+
+		FVector Direction = TargetActor->GetActorLocation() - MuzzleLocation;
+		FRotator MuzzleRotation = Direction.Rotation();
+
+		FActorSpawnParameters Params;
+		Params.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AlwaysSpawn;
+
+		AActor* spawnedProjectile = GetWorld()->SpawnActor<AActor>(ProjectileClass,MuzzleLocation,MuzzleRotation,Params);
+
+		return (spawnedProjectile ? EBTNodeResult::Succeeded : EBTNodeResult::Failed);
+	}
+	
+	return EBTNodeResult::Failed;
+	
+}
